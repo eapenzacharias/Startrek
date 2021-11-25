10f7496836890a2986ac3f22ba4f860cf84d6aad
eapenzacharias <eapenzac@gmail.com>
2021-11-25 18:04:14 +0100

    Item counter added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import { getElement } from './scripts/querySelectors.js';
 import { header, footer, appInit } from './scripts/uiInit.js';
 import './style/style.scss';
 
-header();
 getElement('#main').appendChild(footer());
 appInit();
